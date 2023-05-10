b149ec6ce75966e401c4455184d01280f7172e52
Glark398 <klkp.398@gmail.com>
2023-05-10 09:22:14 -0300

    a

--- app/views.py
+++ app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+print()
+
+def index(request):
+    return HttpResponse("Olá... seja bem vindo a enquete")
+
+
+def sobre(request):
+    return HttpResponse("Este é um app de enquete!")
